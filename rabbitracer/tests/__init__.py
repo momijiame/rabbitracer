28986555221eead1726d642e21abcda73eb90a8e
momijiame <amedama.ginmokusei@gmail.com>
2014-02-08 19:14:01 +0900

    テストファイルを移動する

---
+++ rabbitracer/tests/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import nose
+import sys
+
+from nose.tools.trivial import eq_
+
+from rabbitracer import _parse_args
+
+
+class Test_Main(object):
+
+    def test_parse_default(self):
+        sys.argv = ['rabbitracer']
+        args = _parse_args()
+        eq_(args.hostname, 'localhost')
+        eq_(args.username, 'guest')
+        eq_(args.password, 'guest')
+        eq_(args.virtualhost, '/')
+        eq_(args.pretty_print, False)
+
+
+if __name__ == "__main__":
+    nose.main(argv=['nosetests', '-s', '-v'], defaultTest=__file__)
