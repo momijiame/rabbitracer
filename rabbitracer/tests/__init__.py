22e9d9df17eebd64f5ae4cd7444225f2d2c63f33
momijiame <amedama.ginmokusei@gmail.com>
2014-02-08 19:37:59 +0900

    接続 URI のテストを追加する

--- rabbitracer/tests/__init__.py
+++ rabbitracer/tests/__init__.py
@@ -5,7 +5,7 @@ import sys
 
 from nose.tools.trivial import eq_
 
-from rabbitracer import _parse_args
+from rabbitracer import _parse_args, _build_uri
 
 
 class Test_Main(object):
@@ -19,6 +19,11 @@ class Test_Main(object):
         eq_(args.virtualhost, '/')
         eq_(args.pretty_print, False)
 
+    def test_build_uri(self):
+        sys.argv = ['rabbitracer']
+        args = _parse_args()
+        uri = _build_uri(args)
+        eq_(uri, 'amqp://guest:guest@/')
 
 if __name__ == "__main__":
     nose.main(argv=['nosetests', '-s', '-v'], defaultTest=__file__)
